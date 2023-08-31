19a66a62b4c130fb0d1ee10e9ba35c79b3f8616b
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2023-08-31 11:21:26 +0300

    enemy spawner

---
+++ Submarine_Defence/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        foreach (Transform spawnPoint in spawnPoints)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
